c9dab6c8c7365a700d328f9edbb304c5e3880482
nbnguyen75 <nbnguyen.dev@gmail.com>
2026-06-13 21:07:46 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
-import { exit } from "process";
 import { createInterface } from "readline";
+
+const commands = ['exit', 'echo', 'type']
 
 const rl = createInterface({
   input: process.stdin,
@@ -14,10 +15,18 @@ rl.on('line', (command) => {
   if (command.startsWith('exit'))
   {
     rl.close();
-    return;
+    return process.exit(0);
   }
   else if (command.startsWith('echo '))
     console.log(command.slice(5))
+  else if (command.startsWith('type '))
+  {
+    const givenCommand = command.slice(5).trim()
+    if (commands.includes(givenCommand))
+      console.log(`${givenCommand} is a shell builtin`)
+    else 
+      console.log(`${givenCommand}: not found`)
+  }
   else
     console.log(`${command}: command not found`)
 
